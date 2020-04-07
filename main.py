4934c1e78c05ba7d6cfdf8da4f70ba2342a335b6
Thomas Smullen <thomas.smullen21@bcp.org>
2020-04-07 14:00:48 -0700

    platforms/gravity

--- main.py
+++ main.py
@@ -2,12 +2,21 @@
 # Jumpy! (a platform game) - Part 2
 # Video link: https://www.youtube.com/watch?v=8LRI0RLKyt0
 # Player movement
-#importing libraries and other files
+# © 2019 KidsCanCode LLC / All rights reserved.
+
+# Week of march 23 - Lore
+# Modularity, Github, import as
+# Modularity is file dependencies
+# Classes, methods, functions, data types, ...
+
 import pygame as pg
+from pygame.sprite import Group
+# from pg.sprite import Group
 import random
 from settings import *
 from sprites import *
 
+# this is the game class, we create a new game at the bottom of the code...
 class Game:
     def __init__(self):
         # initialize game window, etc
@@ -20,10 +29,29 @@ class Game:
 
     def new(self):
         # start a new game
-        self.all_sprites = pg.sprite.Group()
-        self.player = Player()
+        self.all_sprites = Group()
+        self.platforms = pg.sprite.Group()
+        self.player = Player(self)
         self.all_sprites.add(self.player)
+        ground = Platform(0, HEIGHT-40, WIDTH, 40)
+        plat1 = Platform(200, 400, 150, 20)
+        # plat2 = Platform(150, 300, 150, 20)
+        # plat3 = Platform(10, 200, 400, 20)
+        # self.all_sprites.add(ground)
+        # self.platforms.add(ground)
+        self.all_sprites.add(plat1)
+        # self.platforms.add(plat1)
+        # self.all_sprites.add(plat2)
+        # self.platforms.add(plat2)
+        # # you need to add new instances of the platform class to groups or it wont update or draw
+        # self.all_sprites.add(plat3)
+        # self.platforms.add(plat3)
+        for plat in range(1,10):
+            plat = Platform(random.randint(0, WIDTH), random.randint(0, HEIGHT), 200, 20)
+            self.all_sprites.add(plat)
+            self.platforms.add(plat)
         self.run()
+
 
     def run(self):
         # Game Loop
@@ -37,6 +65,19 @@ class Game:
     def update(self):
         # Game Loop - Update
         self.all_sprites.update()
+        hits = pg.sprite.spritecollide(self.player, self.platforms, False)
+        if hits:
+            if self.player.rect.top > hits[0].rect.top:
+                print("i hit my head")
+                self.player.vel.y = 15
+                self.player.rect.top = hits[0].rect.bottom + 5
+                self.player.hitpoints -= 10
+                print(self.player.hitpoints)
+            # print("it collided")
+            else:
+                self.player.vel.y = 0
+                self.player.pos.y = hits[0].rect.top+1
+            
 
     def events(self):
         # Game Loop - events
